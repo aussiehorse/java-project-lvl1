9b3dcd088611ec3fedca2c929cb2fc13a325642c
aussiehorse <smautgifter@yahoo.com>
2022-08-08 19:27:14 +0300

    Step 3

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,7 @@
+package hexlet.code;
+
+public class Cli {
+    public static void main() {
+        System.out.println("Welcome to the Brain Games!\nMay I have your name? John\nHello, John!");
+    }
+}
